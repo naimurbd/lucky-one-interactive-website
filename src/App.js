c8b7ab4651fbf43f683651cdef6784c4822946c0
naimurbd <naiemnaiem072@gmail.com>
2022-03-26 00:06:58 +0600

    fake data create and found product length

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Shop from './Components/Shop/Shop';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello react</h1>
+    <div >
+      <Shop></Shop>
     </div>
   );
 }
